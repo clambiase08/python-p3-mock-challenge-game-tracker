999191535934c624e1a49b27ec9ea3447ae4ffa4
Christina Lambiase <christina.lambiase@gmail.com>
2023-07-31 20:06:25 -0700

    finished Result attributes and properties

--- lib/classes/result.py
+++ lib/classes/result.py
@@ -1,3 +1,7 @@
+from classes.player import Player
+from classes.game import Game
+
+
 class Result:
     all = []
 
@@ -16,3 +20,25 @@ class Result:
             self._score = value
         else:
             raise Exception("Score must be an integer between 0 and 5000")
+
+    @property
+    def player(self):
+        return self._player
+
+    @player.setter
+    def player(self, player):
+        if isinstance(player, Player):
+            self._player = player
+        else:
+            raise Exception("Player must be a Player object")
+
+    @property
+    def game(self):
+        return self._game
+
+    @game.setter
+    def game(self, game):
+        if isinstance(self, Game):
+            self._game = game
+        else:
+            raise Exception("Game must be a Game object")
