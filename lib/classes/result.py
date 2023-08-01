68a431225baafc399e430ed679052c56c2e23b27
Christina Lambiase <christina.lambiase@gmail.com>
2023-07-31 20:02:50 -0700

    init and property for result

--- lib/classes/result.py
+++ lib/classes/result.py
@@ -1,8 +1,18 @@
 class Result:
-
     all = []
 
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value):
+        if isinstance(value, int) and 1 <= value <= 5000:
+            self._score = value
+        else:
+            raise Exception("Score must be an integer between 0 and 5000")
