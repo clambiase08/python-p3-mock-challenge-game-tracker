36f0561e3e7040aeea888a89d040370382aa0a89
Christina Lambiase <christina.lambiase@gmail.com>
2023-08-02 09:59:42 -0700

    complete

--- lib/classes/game.py
+++ lib/classes/game.py
@@ -15,28 +15,26 @@ class Game:
     def title(self, value):
         # if hasattr(value, "title"):
         #     raise Exception("Title already set")
-        if isinstance(value, str) and len(value) > 0:
+        if isinstance(value, str) and len(value) > 0 and not hasattr(self, "title"):
             self._title = value
         else:
             raise Exception("Title must be a string")
 
-    def results(self, new_result=None):
+    def results(self):
         from classes.result import Result
 
         return [result for result in Result.all if result.game == self]
 
-    def players(self, new_player=None):
-        from classes.player import Player
-
-        return [result.player for result in self.results() if result.game == self]
+    def players(self):
+        return [result.player for result in self.results()]
 
     def average_score(self, player):
-        total_score = sum(
-            [result.score for result in self.results() if result.game == self]
-        )
-        num_scores = len(self.results())
-        average_score = total_score / num_scores
-        return average_score
+        total_score = [
+            result.score for result in self.results() if result.player == player
+        ]
+        if total_score:
+            return sum(total_score) / len(total_score)
+        return 0
 
     def __repr__(self):
         return f"<Game: {self.title}>"
--- lib/classes/player.py
+++ lib/classes/player.py
@@ -23,49 +23,48 @@ class Player:
 
         return [result for result in Result.all if result.player == self]
 
-    def games_played(self, new_game=None):
-        from classes.game import Game
-
-        return [result.game for result in self.results() if result.player == self]
+    def games_played(self):
+        return [result.game for result in self.results()]
 
     def played_game(self, game):
-        return any(
-            [
-                result
-                for result in self.results()
-                if result.player == self and result.game == game
-            ]
-        )
+        # return any([result for result in self.results() if result.game == game])
+        return game in self.games_played()
 
     def num_times_played(self, game):
-        return len(
-            [
-                result
-                for result in self.results()
-                if result.player == self and result.game == game
-            ]
-        )
+        return len([result for result in self.results() if result.game == game])
 
     @classmethod
     def highest_scored(cls, game):
-        players = {}
-        for player in cls.all:
-            if player.played_game(game):
-                total_score = sum(
-                    [result.score for result in player.results() if result.game == game]
-                )
-                num_scores = player.num_times_played(game)
+        # players = {}
+        # for player in cls.all:
+        #     if player.played_game(game):
+        #         total_score = sum(
+        #             [result.score for result in player.results() if result.game == game]
+        #         )
+        #         num_scores = player.num_times_played(game)
 
-                if num_scores > 0:
-                    average_score = total_score / num_scores
-                else:
-                    average_score = 0
-                players[player] = average_score
+        #         if num_scores > 0:
+        #             average_score = total_score / num_scores
+        #         else:
+        #             average_score = 0
+        #         players[player] = average_score
 
-        if not players:
-            return None
+        # if not players:
+        #     return None
 
-        return max(players)
+        # return max(players)
+
+        # if cls.all:
+        #     max_player = None
+        #     max_score = 0
+        #     for player in cls.all:
+        #         if game.average_score(player) > max_score:
+        #             max_player = player
+        #             max_score = game.average_score(player)
+        #     return max_player
+        # return None
+
+        return max(cls.all, key=lambda player: game.average_score(player))
 
     def __repr__(self):
         return f"<Player: {self.username}>"
